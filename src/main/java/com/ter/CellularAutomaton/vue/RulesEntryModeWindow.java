5672da1d48232eae9bc0af8aabaf8039c01e2d18
alexandreauda <alexandreauda@yahoo.fr>
2017-03-22 00:28:02 +0100

    [UPDATE] Reformat code of Class RulesEntryModeWindow

--- src/main/java/com/ter/CellularAutomaton/vue/RulesEntryModeWindow.java
+++ src/main/java/com/ter/CellularAutomaton/vue/RulesEntryModeWindow.java
@@ -1,7 +1,5 @@
 package com.ter.CellularAutomaton.vue;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -11,7 +9,17 @@ import javax.swing.JButton;
 public class RulesEntryModeWindow {
 
 	private JFrame m_frame;
-
+	private JPanel m_mainPanel;
+	private JPanel m_panelElementary;
+	private JButton m_buttonElementary;
+	private JPanel m_panelAdditive;
+	private JButton m_buttonAdditive;
+	private JPanel m_panelPattern;
+	private JButton m_buttonPattern;
+	private JPanel m_panelScript;
+	private JButton m_buttonScript;
+	
+	
 
 	/**
 	 * Create the application.
@@ -30,37 +38,92 @@ public class RulesEntryModeWindow {
 		m_frame.setLocationRelativeTo(null);
 		m_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		m_frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		buildPanelMain();//Build the main panel
+	}
+	
+	/******Build Main Panel******/
+	//Build the main panel
+	private void buildPanelMain(){
+		m_mainPanel = new JPanel();
+		m_frame.getContentPane().add(m_mainPanel, BorderLayout.CENTER);
+		m_mainPanel.setLayout(new BoxLayout(m_mainPanel, BoxLayout.Y_AXIS));
 		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		panel_1.setLayout(new BorderLayout(0, 0));
+		buildComponentPanelMain();//Build the component of main panel
+	}
+	
+	//Build the component of main panel
+	private void buildComponentPanelMain(){
+		buildPanelElementary();//Build the Elementary panel
 		
-		JButton btnNewButton = new JButton("Elementary");
-		panel_1.add(btnNewButton, BorderLayout.CENTER);
+		buildPanelAdditive();//Build the Additive panel
 		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2);
-		panel_2.setLayout(new BorderLayout(0, 0));
+		buildPanelPattern();//Build the Pattern panel
 		
-		JButton btnNewButton_1 = new JButton("Additive");
-		panel_2.add(btnNewButton_1, BorderLayout.CENTER);
+		buildPanelScript();//Build the Script panel
+	}
+	
+	/******Build Tab Elementary******/
+	//Build the Elementary panel
+	private void buildPanelElementary(){
+		m_panelElementary = new JPanel();
+		m_mainPanel.add(m_panelElementary);
+		m_panelElementary.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_3 = new JPanel();
-		panel.add(panel_3);
-		panel_3.setLayout(new BorderLayout(0, 0));
+		buildComponentPanelElementary();//Build the component of Elementary panel
+	}
+	
+	//Build the component of Elementary panel
+	private void buildComponentPanelElementary(){		
+		m_buttonElementary = new JButton("Elementary");
+		m_panelElementary.add(m_buttonElementary, BorderLayout.CENTER);
+	}
+	
+	/******Build Tab Additive******/
+	//Build the Additive panel
+	private void buildPanelAdditive(){
+		m_panelAdditive = new JPanel();
+		m_mainPanel.add(m_panelAdditive);
+		m_panelAdditive.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNewButton_2 = new JButton("Pattern");
-		panel_3.add(btnNewButton_2, BorderLayout.CENTER);
+		buildComponentPanelAdditive();//Build the component of Additive panel
+	}
+	
+	//Build the component of Additive panel
+	private void buildComponentPanelAdditive(){		
+		m_buttonAdditive = new JButton("Additive");
+		m_panelAdditive.add(m_buttonAdditive, BorderLayout.CENTER);
+	}
+	
+	/******Build Tab Pattern******/
+	//Build the Pattern panel
+	private void buildPanelPattern(){
+		m_panelPattern = new JPanel();
+		m_mainPanel.add(m_panelPattern);
+		m_panelPattern.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_4 = new JPanel();
-		panel.add(panel_4);
-		panel_4.setLayout(new BorderLayout(0, 0));
+		buildComponentPanelPattern();//Build the component of Pattern panel
+	}
+	
+	//Build the component of Pattern panel
+	private void buildComponentPanelPattern(){		
+		m_buttonPattern = new JButton("Pattern");
+		m_panelPattern.add(m_buttonPattern, BorderLayout.CENTER);
+	}
+	
+	/******Build Tab Script******/
+	//Build the Script panel
+	private void buildPanelScript(){
+		m_panelScript = new JPanel();
+		m_mainPanel.add(m_panelScript);
+		m_panelScript.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNewButton_3 = new JButton("Script");
-		panel_4.add(btnNewButton_3, BorderLayout.CENTER);
+		buildComponentPanelScript();//Build the component of Script panel
+	}
+	
+	//Build the component of Script panel
+	private void buildComponentPanelScript(){		
+		m_buttonScript = new JButton("Script");
+		m_panelScript.add(m_buttonScript, BorderLayout.CENTER);
 	}
 
 }
