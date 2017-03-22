c36322cf77c3f9f7b03634ee97556ac92f6ee2f3
alexandreauda <alexandreauda@yahoo.fr>
2017-03-22 20:26:01 +0100

    [UPDATE] Comment code to appear Simulator2D in Main Class + Begin to build ElementaryRulesWindow

---
+++ src/main/java/com/ter/CellularAutomaton/vue/ElementaryRulesWindow.java
@@ -0,0 +1,92 @@
+package com.ter.CellularAutomaton.vue;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import com.ter.CellularAutomaton.controller.CloseElementaryRulesWindowEvent;
+import com.ter.CellularAutomaton.controller.QuitEvent;
+
+public class ElementaryRulesWindow extends JFrame {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/******ATTRIBUTES******/
+	private JMenuBar m_menuBar = new JMenuBar();
+
+	/** The menu file. */
+	//Tab in MenuBar
+	private JMenu m_menuFile = new JMenu("File");
+	
+	/** The menu bar file item 1. */
+	// In file menu
+
+	/** The menu bar file item 1. */
+	private JMenuItem m_menuBarFileItem1 = new JMenuItem("Close");
+
+	/** The menu bar file item 2. */
+	private JMenuItem m_menuBarFileItem2 = new JMenuItem("Quit");
+
+
+	/**
+	 * Create the application.
+	 */
+	public ElementaryRulesWindow() {
+		
+		buildComponentWindow();//Buil component of window
+		
+		this.initMenuBar();//We initialize our menuBar
+		
+		//Set the window visible
+		this.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void buildComponentWindow() {
+		this.setTitle("Elementary Rules Window");
+		this.setSize(480,310);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		
+	}
+	
+	/**
+	 * Inits the menu bar.
+	 */
+	private void initMenuBar(){
+		//We initialize our menuBar 
+		this.constructTabFileMenuBar();//Construction of the tab "File" of menuBar
+		
+		this.setJMenuBar(m_menuBar);//the MenuBar tab is added to Window
+	}
+	
+	/**
+	 * Construct tab file menu bar.
+	 */
+	private void constructTabFileMenuBar(){
+		//Construction of the tab "File" of menuBar
+		this.m_menuFile.add(m_menuBarFileItem1);//Adding a tab "Close" in tab File in MenuBar
+		this.m_menuFile.add(m_menuBarFileItem2);//Adding a tab "Quit" in tab File in MenuBar
+		this.m_menuBar.add(m_menuFile);//the File tab is added to MenuBar
+	}
+	
+	
+	/******Listeners MenuBar******/
+
+	private void addListenerMenuBar (){
+		this.addListenerFile(); //add listener of tab File
+	}
+
+	private void addListenerFile (){
+		m_menuBarFileItem1.addActionListener(new CloseElementaryRulesWindowEvent(this));
+		m_menuBarFileItem2.addActionListener(new QuitEvent());
+	}
+
+}
