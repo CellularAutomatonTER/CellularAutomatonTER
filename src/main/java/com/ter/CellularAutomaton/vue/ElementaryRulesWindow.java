73ef00cfd39a54e09d5fb76c2186ae2d3fb2cf50
alexandreauda <alexandreauda@yahoo.fr>
2017-03-23 23:46:39 +0100

    [UPDATE] Change location of components in window ElementaryRulesWindow

--- src/main/java/com/ter/CellularAutomaton/vue/ElementaryRulesWindow.java
+++ src/main/java/com/ter/CellularAutomaton/vue/ElementaryRulesWindow.java
@@ -54,14 +54,12 @@ public class ElementaryRulesWindow extends JFrame {
 	private final JPanel panel_1 = new JPanel();
 	private final JButton btnReset = new JButton("Reset");
 	private final JCheckBox chckbxNewCheckBox = new JCheckBox("Set Actual Setting By Default");
-	private final JButton btnOk = new JButton("OK");
-	private final Component rigidArea = Box.createRigidArea(new Dimension(80, 20));
-	private final Component rigidArea_1 = Box.createRigidArea(new Dimension(80, 20));
 	private final JPanel panel_2 = new JPanel();
 	private final JLabel label = new JLabel("Alphabet (0 to ?):");
 	private final JFormattedTextField formattedTextField = new JFormattedTextField();
 	private final JLabel label_1 = new JLabel("Rules (0 to 255):");
 	private final JFormattedTextField formattedTextField_1 = new JFormattedTextField();
+	private final JButton btnOk = new JButton("OK");
 
 
 	/**
@@ -86,7 +84,7 @@ public class ElementaryRulesWindow extends JFrame {
 	 */
 	private void buildComponentWindow() {
 		this.setTitle("Elementary Rules Window");
-		this.setSize(520,320);
+		this.setSize(470,320);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
@@ -107,36 +105,22 @@ public class ElementaryRulesWindow extends JFrame {
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 					.addGap(15)
-					.addComponent(btnReset)
-					.addGap(5)
-					.addComponent(rigidArea, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(5)
+					.addComponent(btnReset, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
+					.addGap(36)
 					.addComponent(chckbxNewCheckBox)
-					.addGap(5)
-					.addComponent(rigidArea_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(5)
-					.addComponent(btnOk, GroupLayout.DEFAULT_SIZE, 53, Short.MAX_VALUE)
-					.addContainerGap())
+					.addGap(27)
+					.addComponent(btnOk, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(34, Short.MAX_VALUE))
 		);
 		gl_panel_1.setVerticalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(5)
-							.addComponent(btnReset))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(6)
-							.addComponent(rigidArea, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(5)
-							.addComponent(chckbxNewCheckBox))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(6)
-							.addComponent(rigidArea_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(5)
-							.addComponent(btnOk)))
+					.addGap(5)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnReset, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+							.addComponent(chckbxNewCheckBox)
+							.addComponent(btnOk, GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		panel_1.setLayout(gl_panel_1);
@@ -158,40 +142,37 @@ public class ElementaryRulesWindow extends JFrame {
 			gl_panel_5.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_5.createSequentialGroup()
 					.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING, false)
-							.addGroup(gl_panel_5.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(gl_panel_5.createParallelGroup(Alignment.LEADING)
-									.addGroup(gl_panel_5.createSequentialGroup()
-										.addComponent(label, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED)
-										.addComponent(formattedTextField, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)
-										.addGap(33)
-										.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
-									.addGroup(gl_panel_5.createSequentialGroup()
-										.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED)
-										.addComponent(formattedTextField_1, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)))
-								.addGap(9569))
-							.addComponent(separator, GroupLayout.DEFAULT_SIZE, 10000, Short.MAX_VALUE))
-						.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 10000, GroupLayout.PREFERRED_SIZE))
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 10000, GroupLayout.PREFERRED_SIZE)
+						.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 10000, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(formattedTextField, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)
+							.addGap(33)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_5.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(formattedTextField_1, GroupLayout.PREFERRED_SIZE, 138, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_5.setVerticalGroup(
 			gl_panel_5.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_5.createSequentialGroup()
-					.addGap(41)
+					.addGap(32)
 					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
 						.addComponent(label)
 						.addComponent(formattedTextField, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
-					.addGap(30)
+						.addComponent(btnNewButton))
+					.addGap(39)
 					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(26)
+					.addGap(33)
 					.addGroup(gl_panel_5.createParallelGroup(Alignment.BASELINE)
 						.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
 						.addComponent(formattedTextField_1, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
 					.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 		);
 		panel_2.setLayout(gl_panel_5);
