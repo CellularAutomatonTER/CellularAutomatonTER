f812380a4845f6a63363a400219363ce865f6bcc
alexandreauda <alexandreauda@yahoo.fr>
2017-03-29 03:11:09 +0200

    [UPDATE] Change label

--- src/main/java/com/ter/CellularAutomaton/vue/ElementaryRulesWindow.java
+++ src/main/java/com/ter/CellularAutomaton/vue/ElementaryRulesWindow.java
@@ -52,7 +52,7 @@ public class ElementaryRulesWindow extends JFrame {
 	private final JLabel m_labelAlphabet = new JLabel("Alphabet (0 to ?):  1");
 	private final JLabel m_labelRadius = new JLabel("Radius (must be equal or greater than 1):");
 	private final JFormattedTextField m_formattedTextFieldRadius = new JFormattedTextField();
-	private final JLabel m_labelRules = new JLabel("Rules (0 to 255):");
+	private final JLabel m_labelRules = new JLabel("Rules (0 to ?):");
 	private final JFormattedTextField m_formattedTextFieldRules = new JFormattedTextField();
 	private final JButton m_buttonOk = new JButton("OK");
 	private final JButton m_buttonPersonalize = new JButton("Personalize");
