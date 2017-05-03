bf32e9a639e3bece2a55606d58ae9e03e895d680
alexandreauda <alexandreauda@yahoo.fr>
2017-05-03 14:18:43 +0200

    [UPDATE] The size of Screen change when we resize simulation

--- src/main/java/com/ter/CellularAutomaton/controller/OKSizeSimulation1DEvent.java
+++ src/main/java/com/ter/CellularAutomaton/controller/OKSizeSimulation1DEvent.java
@@ -1,5 +1,6 @@
 package com.ter.CellularAutomaton.controller;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -40,11 +41,15 @@ public class OKSizeSimulation1DEvent implements ActionListener {
 		stringInputWidthOfSimulation = stringInputWidthOfSimulation.replaceAll("\\s","");//Delete space in string
 		int intInputWidthOfSimulation = convertStringNumberToIntNumber(stringInputWidthOfSimulation);// Convert string number to int number and stock the int number.
 		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_simulation().setm_nbCellWidth(intInputWidthOfSimulation/Cell1D.CELL_SIZE);//Set the new number of cell which define the width of simulation 
+		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_screen().setm_screenWidth(intInputWidthOfSimulation);
 		
 		String stringInputHeightOfSimulation = m_sizeSimulationWindow.getm_formattedTextFieldHeightOfSimulation().getText();
 		stringInputHeightOfSimulation = stringInputHeightOfSimulation.replaceAll("\\s","");//Delete space in string
 		int intInputHeightOfSimulation = convertStringNumberToIntNumber(stringInputHeightOfSimulation);// Convert string number to int number and stock the int number.
 		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_simulation().setm_nbCellHeight(intInputHeightOfSimulation/Cell1D.CELL_SIZE);//Set the new number of cell which define the height of simulation 
+		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_screen().setm_screenHeight(intInputHeightOfSimulation);
+		
+		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_screen().setPreferredSize(new Dimension(m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_screen().getm_screenWidth(), m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_screen().getm_screenHeight()));
 		
 		m_sizeSimulationWindow.getm_mainWindow1D().getm_internalFrameSimulation().getm_simulation().initByDefaultSimulation();
 		m_sizeSimulationWindow.getm_mainWindow1D().setm_simulationState(SimulationState.RUN);
